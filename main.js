f8259998ca19856c8c763acad3aa9a0e52936a8f
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-09-13 19:02:37 +0400

    Add Board class

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    mainWindow.loadFile('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
     mainWindow.on('closed', function() {
         mainWindow = null;
